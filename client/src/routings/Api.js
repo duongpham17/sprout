ae4f22d8075dbdfbee3dac1a7f2e63f82d47d7bc
Duong Pham <duongpham17@icloud.com>
2020-12-01 03:58:56 +0000

    first commit

--- client/src/routings/Api.js
+++ client/src/routings/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: process.env.REACT_APP_SERVER_PORT,
+    baseURL: process.env.NODE_ENV === "development" ? process.env.REACT_APP_DEVELOPMENT_SERVER_PORT : process.env.REACT_APP_PRODUCTION_PORT ,
     withCredentials: true,
     credentials: "include",
 });
