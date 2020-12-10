a19cc57fb7734c6cffba86a2b67eee7b054a35c3
Duong Pham <duongpham17@icloud.com>
2020-12-10 19:10:42 +0000

    punc

--- client/src/routings/AcceptCookie.js
+++ client/src/routings/AcceptCookie.js
@@ -20,7 +20,7 @@ const AcceptTerms = props => {
             <div className="-terms">
             <h2><BiCookie size="1.5rem"/> We use Cookies!</h2>
             <p>Cookies help to run our services and to better your experience. For more information about the cookie read our <Link to="/cookie">Cookie Policy</Link> </p>
-            <p>If your happy with this... </p>
+            <p>If you'r happy with this... </p>
             <button onClick={(e) => accept(e)}>I Accept</button>
             </div>
         </div>
